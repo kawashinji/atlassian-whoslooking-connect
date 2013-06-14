e94207afd151f9c73cbb522505b0411a53813945
Robin Fernandes <rfernandes@atlassian.com>
2013-06-14 14:44:05 +1000

    Remove references to 'remote' in the name (prefer 'connect').

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "whoslooking-remote"
+  val appName         = "whoslooking-connect"
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
