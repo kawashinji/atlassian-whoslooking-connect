6734f832f1d0deb351f8c5393ed3ed436a72abe8
Robin Fernandes <rfernandes@atlassian.com>
2013-06-20 18:23:14 +1000

    Fix up AUI retrieval. Improve temp controller.

--- app/controllers/Temp.java
+++ app/controllers/Temp.java
@@ -1,16 +1,29 @@
 package controllers;
 
+import org.codehaus.jackson.JsonNode;
+
 import play.Logger;
+import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class Temp extends Controller
 {
 
+    @BodyParser.Of(BodyParser.Json.class)
     public static Result capabilities()
     {
-        Logger.info(request().body().asText());
-        return ok();
+        JsonNode requestContent = request().body().asJson();
+        Logger.info(requestContent.toString());
+
+        String[] respondWith = request().queryString().get("respondWith");
+        int status = 200;
+        if (respondWith != null && respondWith.length != 0)
+        {
+            status = Integer.parseInt(respondWith[0]);
+        }
+
+        return status(status, requestContent);
     }
 
 }
