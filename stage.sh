fc4e4fdd5ef0a5b36fcc0c2c27995f02b77afa98
Robin Fernandes <rfernandes@atlassian.com>
2013-06-20 13:15:51 +1000

    add deployment scripts.

---
+++ stage.sh
@@ -0,0 +1,5 @@
+#!/bin/bashi -e
+NEW_RELIC_API_KEY=`heroku config:get NEW_RELIC_API_KEY --remote staging`
+NEW_RELIC_APP_NAME=`heroku config:get NEW_RELIC_APP_NAME --remote staging`
+git push staging staging:master
+curl -H "x-api-key:${NEW_RELIC_API_KEY}" -d "deployment[app_name]=${NEW_RELIC_APP_NAME}" https://rpm.newrelic.com/deployments.xml
