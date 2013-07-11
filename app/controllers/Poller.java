92b9d3264df6719c0646cc1cfd6f54442cd7f7bd
Robin Fernandes <rfernandes@atlassian.com>
2013-07-11 11:34:46 +1000

    Handle anonymous users more gracefully.

--- app/controllers/Poller.java
+++ app/controllers/Poller.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import com.atlassian.connect.play.java.AC;
 import com.atlassian.connect.play.java.CheckValidOAuthRequest;
 
+import org.apache.commons.lang.StringUtils;
 import org.codehaus.jackson.JsonNode;
 
 import play.api.libs.Crypto;
@@ -16,6 +17,8 @@ import service.ViewablesService;
 
 public class Poller extends Controller
 {
+    private static final String ANONYMOUS = "anonymous";
+
     public static final String PER_PAGE_VIEW_TOKEN_HEADER = "x-per-page-view-token";
 
     private final ViewablesService viewables = new RedisViewablesService();
@@ -26,12 +29,19 @@ public class Poller extends Controller
         final String hostId = request().queryString().get("oauth_consumer_key")[0];
         final String resourceId = request().getQueryString("issue_id");
         final String userId = AC.getUser().getOrNull();
-        final String perPageViewToken = Crypto.sign(hostId + userId);
+
+
+        if (StringUtils.isBlank(userId))
+        {
+            return unauthorized(views.html.anonymous.render(hostId, resourceId, userId));
+        }
 
         viewables.putViewer(hostId, resourceId, userId);
         final Map<String, JsonNode> viewersWithDetails = viewables.getViewersWithDetails(resourceId, hostId);
-        return ok(views.html.poller.render(Json.toJson(viewersWithDetails).toString(),
-                                           hostId, resourceId, userId, perPageViewToken));
+
+        final String perPageViewToken = Crypto.sign(hostId + userId);
+        return ok(views.html.poller.render(Json.toJson(viewersWithDetails).toString(), hostId, resourceId, userId, perPageViewToken));
+
     }
 
 }
