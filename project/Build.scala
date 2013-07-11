f049d56ac65526ad6b0a588a2cdd11ddda3989ab
Robin Fernandes <rfernandes@atlassian.com>
2013-07-11 13:06:42 +1000

    Try to add New Relic RUM.

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,8 @@ object ApplicationBuild extends Build {
     "commons-io" % "commons-io" % "2.4",
     "com.typesafe" %% "play-plugins-redis" % "2.1-1-RC2-robinf-3",
     "com.atlassian.connect" % "ac-play-java_2.10" % "0.5.1",
-    "org.apache.commons" % "commons-lang3" % "3.1"
+    "org.apache.commons" % "commons-lang3" % "3.1",
+    "com.newrelic.agent.java" % "newrelic-api" % "2.20.0"    
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
