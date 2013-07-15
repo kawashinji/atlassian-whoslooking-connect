39128f9087afd8e74c9b8296263021e4ecbfb947
Robin Fernandes <rfernandes@atlassian.com>
2013-07-15 11:19:52 +1000

    Upgrade ac-play-java library to latest

--- project/Build.scala
+++ project/Build.scala
@@ -14,7 +14,7 @@ object ApplicationBuild extends Build {
     javaEbean,
     "commons-io" % "commons-io" % "2.4",
     "com.typesafe" %% "play-plugins-redis" % "2.1-1-RC2-robinf-3",
-    "com.atlassian.connect" % "ac-play-java_2.10" % "0.5.1",
+    "com.atlassian.connect" % "ac-play-java_2.10" % "0.5.4",
     "org.apache.commons" % "commons-lang3" % "3.1",
     "com.newrelic.agent.java" % "newrelic-api" % "2.20.0"    
   )
