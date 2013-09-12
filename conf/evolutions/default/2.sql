2a94356a24ebb52d9a83d5dcf3270e452ac926b8
Robin Fernandes <rfernandes@atlassian.com>
2013-09-12 19:18:36 +1000

    Fix evolution script

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,3 +1,11 @@
+# --- !Ups
+
 alter table ac_host rename column base_url to baseurl;
 
 alter table ac_host rename column public_key to publickey;
+
+# --- !Downs
+
+alter table ac_host rename column baseurl to base_url;
+
+alter table ac_host rename column publickey to public_key;
