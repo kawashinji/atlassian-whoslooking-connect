6dfb1bb72c1c764339e3f202c6d30e2e493a21bf
Anders Holmgren <aholmgren@atlassian.com>
2014-01-22 15:19:38 +1100

    review updates

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "whoslooking-connect"
-  val appVersion      = "1.1-SNAPSHOT"
+  val appVersion      = "2.0-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
