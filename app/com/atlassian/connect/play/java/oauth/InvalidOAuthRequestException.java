1724d4a59bcea1e505cb5924d65091b7ba4f6a32
Robin Fernandes <robin@atlassian.com>
2014-03-08 07:57:32 +1100

    Revert "Pull back the OAuth stuff from an older ac-play library to support backwards compatibility."
    This reverts commit a26acf5eec607a53b0284097a5635384dfd23966.

--- app/com/atlassian/connect/play/java/oauth/InvalidOAuthRequestException.java
+++
@@ -1,9 +0,0 @@
-package com.atlassian.connect.play.java.oauth;
-
-public final class InvalidOAuthRequestException extends RuntimeException
-{
-    public InvalidOAuthRequestException(String msg)
-    {
-        super(msg);
-    }
-}
--- app/com/atlassian/connect/play/java/oauth/UnauthorisedOAuthRequestException.java
+++
@@ -1,9 +0,0 @@
-package com.atlassian.connect.play.java.oauth;
-
-public final class UnauthorisedOAuthRequestException extends RuntimeException
-{
-    public UnauthorisedOAuthRequestException(String msg, Throwable throwable)
-    {
-        super(msg, throwable);
-    }
-}
